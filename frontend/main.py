adaac58218c235a74c3dc437f8abed5b721b0a05
Patrick Mediodia <mediodia.patrick@gmail.com>
2025-01-12 15:58:50 +0800

    add UI for editing

--- frontend/main.py
+++ frontend/main.py
@@ -14,11 +14,30 @@ def add_post():
     tags = st.text_input("Tags")
     content = st.text_area("Content")
 
-    if not st.button("Submit"):
+    if st.button("Submit"):
         data = {
             "title" : title,
             "tags" : tags.split(","),
             "content" : content
+        }
+        st.write(data)
+        response = requests.post(f'{API_ENDPOINT}/posts', data=json.dumps(data))
+        if not response.ok:
+            st.write(response.reason)
+        else:
+            st.rerun()
+
+@st.dialog("Edit Post")
+def edit_post(title, tags, content):
+    st.text_input("Title").value = title
+    st.text_input("Tags").value = tags
+    st.text_area("Content").value = content
+
+    if st.button("Submit"):
+        data = {
+            "title" : st.text_input("Title").value,
+            "tags" : st.text_input("Tags").value.split(","),
+            "content" : st.text_area("Content").value
         }
         st.write(data)
         response = requests.post(f'{API_ENDPOINT}/posts', data=json.dumps(data))
@@ -57,11 +76,14 @@ def show_posts():
     data = response.json()
 
     for post in data:
-        col1, col2 = st.columns([20, 1], vertical_alignment="bottom")
+        col1, col2, col3 = st.columns([16, 1, 1.5], vertical_alignment="bottom")
         with col1:
             st.write(f"### {post['title']}")
         with col2:
-            if st.button("x", key=post['id'], type="tertiary"):
+            if st.button("Edit", key= f'{post['id']}/edit', type="tertiary"):
+                edit_post(post['title'], ', '.join(post['tags']), post['content'])
+        with col3:
+            if st.button("Delete", key=f'{post['id']}/delete', type="tertiary"):
                 delete_post(post['id'])
 
         tags = [ f"`{tag}`" for tag in post['tags'] ]
